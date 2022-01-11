f7a601086059223c4a74e19851d2cf9fd8176314
Balbader <15971200+Balbader@users.noreply.github.com>
2022-01-12 00:25:40 +0100

    added increment & save functions

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 let count = 0;
 
-const button = document.getElementById('counter');
-
-document.addEventListener('click', (button) => {
+const increment = () => {
     count += 1;
-    console.log(count);
     document.getElementById('count-el').innerText = count;
-});
+};
+
+const saveCount = () => {
+    document.getElementById('final-count').innerText = count;
+    count = 0;
+    document.getElementById('count-el').innerText = count;
+};
+
+
+//const addButton = document.getElementById('counter');
+
+//document.addEventListener('click', (addButton) => {
+//count += 1;
+//console.log(count);
+//document.getElementById('count-el').innerText = count;
+//});
+
+//const saveButton = document.getElementById('save-btn');
+//document.addEventListener('click', (saveButton) => {
+//count = 0;
+//document.getElementById('count-el').innerText = count;
+//})
