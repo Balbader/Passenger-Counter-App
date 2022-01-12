b3d34d8ae29194b434929b4b5bdfa5e89af640b1
Balbader <15971200+Balbader@users.noreply.github.com>
2022-01-12 20:21:10 +0100

    New code added

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 let count = 0;
 
-const increment = () => {
-    count += 1;
-    document.getElementById('count-el').innerText = count;
-};
-
-const saveCount = () => {
-    document.getElementById('final-count').innerText = count;
-    count = 0;
-    document.getElementById('count-el').innerText = count;
-};
-
-
-//const addButton = document.getElementById('counter');
-
-//document.addEventListener('click', (addButton) => {
+//const increment = () => {
 //count += 1;
-//console.log(count);
 //document.getElementById('count-el').innerText = count;
-//});
+//};
 
-//const saveButton = document.getElementById('save-btn');
-//document.addEventListener('click', (saveButton) => {
+//const saveCount = () => {
+//document.getElementById('final-count').innerText = count;
 //count = 0;
 //document.getElementById('count-el').innerText = count;
-//})
+//};
+
+
+const addButton = document.getElementById('counter');
+
+addButton.addEventListener('click', (addButton) => {
+    count += 1;
+    console.log(count);
+    document.getElementById('count-el').innerText = count;
+});
+
+const saveButton = document.getElementById('save-btn');
+saveButton.addEventListener('click', (saveButton) => {
+    count = 0;
+    document.getElementById('count-el').innerText = count;
+})
