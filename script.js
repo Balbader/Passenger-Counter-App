766c067752f4694c7a943fd70018bc4aa93e0191
Balbader <15971200+Balbader@users.noreply.github.com>
2022-01-12 20:27:09 +0100

    clean up

--- script.js
+++ script.js
@@ -1,27 +1,14 @@
 let count = 0;
 
-//const increment = () => {
-//count += 1;
-//document.getElementById('count-el').innerText = count;
-//};
-
-//const saveCount = () => {
-//document.getElementById('final-count').innerText = count;
-//count = 0;
-//document.getElementById('count-el').innerText = count;
-//};
-
-
 const addButton = document.getElementById('counter');
-
 addButton.addEventListener('click', (addButton) => {
     count += 1;
-    console.log(count);
     document.getElementById('count-el').innerText = count;
 });
 
 const saveButton = document.getElementById('save-btn');
 saveButton.addEventListener('click', (saveButton) => {
+    document.getElementById('final-count').innerText = count;
     count = 0;
     document.getElementById('count-el').innerText = count;
 })
